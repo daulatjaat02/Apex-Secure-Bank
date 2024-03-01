1bd61e09499907c455199c070ed06e3f2817f1c6
daulat <daulatjajra2006@gmail.com>
2024-03-01 17:46:14 +0530

    dom

--- script.js
+++ script.js
@@ -243,16 +243,3 @@ document.querySelectorAll(".operations__tab").forEach((el) => {
 });
 
 //////////////////////////////////////////////////////////////////////
-document.addEventListener("DOMContentLoaded", function () {
-  const menuBtn = document.querySelector(".menu-btn");
-  const sidebar = document.querySelector(".sidebar");
-  const closeBtn = document.querySelector(".close-btn");
-
-  menuBtn.addEventListener("click", function () {
-    sidebar.style.right = "0";
-  });
-
-  closeBtn.addEventListener("click", function () {
-    sidebar.style.right = "-250px";
-  });
-});
